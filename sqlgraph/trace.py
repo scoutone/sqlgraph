b78c4fae354b488a8f58b2c4cc93137ef67abd0f
Brad Arndt <brad.arndt@gmail.com>
2025-01-20 15:12:43 -0600

    add order to tracer

--- sqlgraph/trace.py
+++ sqlgraph/trace.py
@@ -535,7 +535,7 @@ class SqlTrace():
                 return self.trace(e.parent)
             elif type(e) == exp.Column:
                 return self.trace_column(e)
-            elif type(e) in [exp.Alias, exp.Cast, exp.Paren, exp.Max, exp.Min, exp.ArraySize]:
+            elif type(e) in [exp.Alias, exp.Cast, exp.Paren, exp.Max, exp.Min, exp.ArraySize, exp.Order]:
                 return self.trace(e.args['this'])
             elif type(e) == exp.Window:
                 return self.trace_window(e)
